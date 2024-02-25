3ca664cbda9d3fae8ecf2ab3a5361900e6c35c0e
adriensas <adrien.sas@gmail.com>
2024-02-25 22:42:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="alloprompt",
-    packages=find_packages(include=["allprompt"]),
+    packages=find_packages(include=["alloprompt"]),
     version="0.1.0",
     description="",
     author="AlloBrain",
