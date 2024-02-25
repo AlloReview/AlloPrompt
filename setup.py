57362aa695e5f9a3c3a35684f9e443d75aebfb32
adriensas <adrien.sas@gmail.com>
2024-02-25 22:57:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import find_packages, setup
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name="alloprompt",
     packages=find_packages(include=["alloprompt"]),
     version="0.1.0",
     description="",
     author="AlloBrain",
-    install_requires=[],
+    install_requires=required,
 )
