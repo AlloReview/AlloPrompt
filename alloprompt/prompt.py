73486f0769740923f4d6ccf97e2cdfa90aeadb9e
adriensas <adrien.sas@gmail.com>
2024-05-06 15:15:30 +0200

    fix functions in components

--- alloprompt/prompt.py
+++ alloprompt/prompt.py
@@ -75,7 +75,7 @@ class Prompt:
         return render_jinja2(
             template,
             **data,
-            render=lambda t, d: self.render(t, **d),
+            render=lambda t, d: self.render(t, functions=self.functions, **d),
             otag=otag,
             ctag=ctag,
             to_yaml=convert_dict_to_yaml,
@@ -100,8 +100,12 @@ class Prompt:
             print(rendered_prompt)
             raise e
         if debug:
-            print("Messages:")
-            print(convert_dict_to_yaml(rendered_prompt["messages"]))
+            if "messages" in rendered_prompt:
+                print("Messages:")
+                print(convert_dict_to_yaml(rendered_prompt["messages"]))
+            if "text_prompt" in rendered_prompt:
+                print("Prompt:")
+                print(rendered_prompt["text_prompt"])
         return rendered_prompt
 
     def chat_complete(
@@ -127,6 +131,35 @@ class Prompt:
                 return iterate_responses(response)
             else:
                 response = response.choices[0].message.content
+                result = self.reverse_template(response, self.template["output_template"], self.cache)
+                if output_as_yaml:
+                    return convert_dict_to_yaml(result)
+                else:
+                    return result
+        except Exception as e:
+            raise Exception(f'Error "{e}" while parsing the response:\n{response}')
+
+    def complete(self, inputs=None, inputs_yaml=None, client=None, debug=False, output_as_yaml=False, *args, **kwargs):
+        if client is None:
+            client = self.default_client
+        rendered_prompt = self.render_prompt(inputs, inputs_yaml, debug)
+        chat_complete_args = {**self.default_chat_complete_args, **kwargs}
+        response = client.completions.create(prompt=rendered_prompt["text_prompt"], *args, **chat_complete_args)
+        if debug:
+            print("Response:")
+            print(response)
+        try:
+            if chat_complete_args.get("stream", False):
+
+                def iterate_responses(generator):
+                    response_text = ""
+                    for response in generator:
+                        response_text += response.choices[0].delta.text or ""
+                        yield self.stream_output_parsing_function(response_text)
+
+                return iterate_responses(response)
+            else:
+                response = response.choices[0].text
                 result = self.reverse_template(response, self.template["output_template"], self.cache)
                 if output_as_yaml:
                     return convert_dict_to_yaml(result)
